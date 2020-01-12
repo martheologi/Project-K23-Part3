da12152331b0605c3deb290230df14572e1edda7
martheologi <martheo1708@gmail.com>
2020-01-12 21:28:15 +0200

    A_B

--- new_representation.py
+++ new_representation.py
@@ -4,17 +4,31 @@ import math
 import sys
 import pandas as pd
 import numpy as np
+from pandas import DataFrame
 from keras.models import load_model
-from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import mean_squared_error
 from keras import layers, optimizers, losses, metrics
 
 data = pd.read_csv(sys.argv[1], sep=',', header=None, usecols=[*range(1, 129)])
-#print(data)
+timestamps = pd.read_csv(sys.argv[1], sep=',', header=None, usecols=[*range(0, 1)])
+timestamps = np.array(timestamps)
 
 model = load_model('WindDenseNN.h5')
 
-# summarize the structure of the model
-model.summary()
+print(model.layers[0].output)
 
-print(model.layers[1])
+new_model = keras.Sequential()
+
+new_model.add(model.layers[0])
+
+new_model.compile(optimizer=optimizers.RMSprop(0.01), loss=losses.CategoricalCrossentropy(), metrics=[metrics.CategoricalAccuracy()])
+
+result = new_model.predict(data, batch_size=32)
+
+df = pd.DataFrame(result)
+df.to_csv('output.csv', header=False, index=False)
+
+print(result.shape)
+print(timestamps.shape)
+
+print(timestamps)
+print(result)
