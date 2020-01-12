56faf7fb51671e7397d5f3d9eb62b541174eebf1
martheologi <martheo1708@gmail.com>
2020-01-12 23:46:29 +0200

    A-B

--- new_representation.py
+++ new_representation.py
@@ -2,6 +2,7 @@ import keras
 import tensorflow
 import math
 import sys
+import csv
 import pandas as pd
 import numpy as np
 from pandas import DataFrame
@@ -14,8 +15,6 @@ timestamps = np.array(timestamps)
 
 model = load_model('WindDenseNN.h5')
 
-print(model.layers[0].output)
-
 new_model = keras.Sequential()
 
 new_model.add(model.layers[0])
@@ -24,11 +23,8 @@ new_model.compile(optimizer=optimizers.RMSprop(0.01), loss=losses.CategoricalCro
 
 result = new_model.predict(data, batch_size=32)
 
-df = pd.DataFrame(result)
-df.to_csv('output.csv', header=False, index=False)
+final = np.append(timestamps, result, axis=1)
 
-print(result.shape)
-print(timestamps.shape)
-
-print(timestamps)
-print(result)
+with open("repres_output.csv","w") as my_csv:
+    csvWriter = csv.writer(my_csv, delimiter=',')
+    csvWriter.writerows(final)
--- predict.py
+++ predict.py
@@ -2,6 +2,7 @@ import keras
 import tensorflow
 import math
 import sys
+import csv
 import pandas as pd
 import numpy as np
 from keras.models import load_model
@@ -10,22 +11,34 @@ from sklearn.metrics import mean_squared_error
 from keras import layers, optimizers, losses, metrics
 
 data = pd.read_csv(sys.argv[1], sep=',', header=None, usecols=[*range(1, 129)])
-#print(data)
+timestamps = pd.read_csv(sys.argv[1], sep=',', header=None, usecols=[*range(0, 1)])
+timestamps = np.array(timestamps)
 
 model = load_model('WindDenseNN.h5')
 
 # summarize the structure of the model
-model.summary()
+#model.summary()
 
 result = model.predict(data, batch_size=32)
-#print(result.shape)
 
 actual_data = pd.read_csv('actual.csv', sep=',', header=None, usecols=[*range(1, 8)])
 actual_data = np.array(actual_data)
 
 MAE = mean_absolute_error(actual_data, result)
 MSE = mean_squared_error(actual_data, result)
+MAPE = np.mean(np.abs((actual_data - result) / np.mean(actual_data))) * 100
 
 print("MAE = ", MAE)
 print("MSE = ", MSE)
-print("MAPE = ", np.mean(np.abs((actual_data - result) / np.mean(actual_data))) * 100)
+print("MAPE = ", MAPE)
+
+output_file = open('predict_output.csv', 'w')
+errors = '%s%f%s%f%s%f \n' % ("MAE = ", MAE, " MAPE = ", MAPE, " MSE = ", MSE)
+output_file.write(errors)
+output_file.close()
+
+final = np.append(timestamps, result, axis=1)
+
+with open("predict_output.csv","a+") as my_csv:
+    csvWriter = csv.writer(my_csv, delimiter=',')
+    csvWriter.writerows(final)
